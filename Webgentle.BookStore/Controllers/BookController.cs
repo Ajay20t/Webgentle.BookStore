78db73314006f703cd88fdce851744655d3d8769
Ajay20t <ajay@DESKTOP-4DSVOKS>
2023-08-22 10:50:37 +0530

    part 35

--- Webgentle.BookStore/Controllers/BookController.cs
+++ Webgentle.BookStore/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Dynamic;
 using Webgentle.BookStore.Models;
 using Webgentle.BookStore.Models.Repository;
 
@@ -10,6 +11,7 @@ namespace Webgentle.BookStore.Controllers
         private readonly BookRepository _bookRepository = null;
         public BookController()
         {
+            ViewBag.Title = "Book Ajay";
             _bookRepository = new BookRepository(); 
         }
         public ViewResult GetAllBooks()
@@ -19,7 +21,10 @@ namespace Webgentle.BookStore.Controllers
         }
         public ViewResult GetBook(int id)
         {
-            var data =  _bookRepository.GetBookById(id);
+            dynamic data = new ExpandoObject();
+            data.book = _bookRepository.GetBookById(id);
+            data.name = "Ajay";
+            //var data =  _bookRepository.GetBookById(id);
             return View(data);
         }
         public List<BookModel> searchBook(string bookname, string Authorname) 
--- Webgentle.BookStore/Controllers/HomeController.cs
+++ Webgentle.BookStore/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using Webgentle.BookStore.Models;
 
 namespace Webgentle.BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string CustomProperty { get; set; }
+        [ViewData]
+        public string Title { get; set; }
         public ViewResult Index()
         {
-           // return View("TempView/Tempindex.cshtml");
-           return View();
+            /*ViewBag.Title = "Ajay";
+            dynamic data = new ExpandoObject();
+            data.Title = "Ajay";
+            data.id = 1;
+            ViewBag.Data = data;
+            ViewBag.Type = new BookModel() { id = 5, Author = "This is Author" };*/
+
+            //ViewData["Title"] = "Title";
+            //ViewData["Book"]= new BookModel() { id = 5, Author = "This is Author" };
+            CustomProperty = "Custom Value";
+            Title = "Home page";
+
+            return View();
         }
         public ViewResult AboutUs()  
         {
